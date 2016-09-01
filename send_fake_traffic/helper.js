6efe83b7ae88b9f7066f2cfe38d92bfe359f3514
Guillaume Verbal <guillaume.verbal@gmail.com>
2016-09-01 13:13:55 +0800

    add fake traffic generator

--- send_fake_traffic/helper.js
+++ send_fake_traffic/helper.js
@@ -2,19 +2,50 @@
 
 var userAgentHelper = require('./user_agent');
 
-module.exports.getRandomUserAgent = userAgentHelper.getRandom();
+module.exports.getRandomUserAgent = userAgentHelper.getRandom;
 
-// give 'GET' a little more weight
-var httpMethods = [ 'GET', 'GET', 'GET', 'GET', 'POST' ];
+var httpMethods = [
+    'GET', 'GET', 'GET', 'GET', // give 'GET' a little more weight
+    'POST',
+];
 module.exports.getRandomHTTPMethod = function ()
 {
     return httpMethods[Math.floor(Math.random() * httpMethods.length)];
 };
 
-// give '/' a little more weight
-var uris = [ '/', '/', '/', '/login', '/register', '/blog', 'user' ];
-module.exports.getRandomHTTPMethod = function ()
+var uris = [
+    '/', '/', '/', // give '/' a little more weight
+    '/login',
+    '/register',
+    '/blog',
+    '/user',
+    '/non-existant-page', // to get some 404
+];
+module.exports.getRandomURI = function ()
 {
     return uris[Math.floor(Math.random() * uris.length)];
 };
 
+var cookies = [];
+
+// will generate 100 random cookies
+(function ()
+{
+    var char = '0123456789abcdef';
+    var i, j, temp;
+    for (i = 0; i < 100; i++)
+    {
+        temp = '';
+        for (j = 0; j < 32; j++)
+            temp += char[Math.floor(Math.random() * char.length)];
+
+        cookies.push(temp);
+    }
+
+    console.log(cookies);
+})(); // direct call
+
+module.exports.getRandomCookie = function ()
+{
+    return cookies[Math.floor(Math.random() * cookies.length)];
+};
--- send_fake_traffic/index.js
+++ send_fake_traffic/index.js
@@ -1,2 +1,22 @@
 'use strict';
 
+var request = require('request');
+var helper = require('./helper');
+
+setInterval(function ()
+{
+    request({
+        baseUrl: 'http://localhost:3000/',
+        uri: helper.getRandomURI(),
+        method: helper.getRandomHTTPMethod(),
+        headers: {
+            'User-Agent': helper.getRandomUserAgent(),
+            Cookie: helper.getRandomCookie(),
+        },
+        timeout: 1000, // ms
+    }, function (error)
+    {
+        if (error)
+            console.dir(error);
+    });
+}, 100); // ~10 times per second
